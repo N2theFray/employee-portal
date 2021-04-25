1eaa363657af1c2942ae10a5c35717780b15a2f7
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-04-25 12:23:00 +0300

    almost mvp

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
 
+
 const employee = [];
 var userRole = 'Office Number'
 
@@ -77,12 +78,96 @@ function addMember () {
         
 }
 
+function startHTML (){
+    const starterHTML =
+    `<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>My Team</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-info mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`
+    
+    fs.writeFile("./dist/index.html", starterHTML, function(err){
+        if (err){
+            console.log(err)
+        }
+    })
+    
+}
 
+function addHTML(teamMember) {
+    return new Promise(function() {
+        const name = teamMember.getName();
+        const role = teamMember.getRole();
+        const id = teamMember.getId();
+        const email = teamMember.getEmail();
+        let data = ''
 
-function addHTML(){
+        if (role === "Manager") {
+            const officePhone = teamMember.getOffice();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h4 class="card-header font-weight-bold bg-primary">${name}<br><br> <span class="font-weight-light font-italic">Manager</span></h4>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <br><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">Office Phone: <br>${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        } else if (role === "Intern") {
+            const school = teamMember.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h4 class="card-header font-weight-bold bg-secondary">${name}<br><br> <span class="font-weight-light font-italic">Intern</span></h4>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <br><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`
+        } else {
+            const gitHub = teamMember.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h4 class="card-header font-weight-bold bg-secondary">${name}<br><br> <span class="font-weight-light font-italic">Engineer</span></h4>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: <br><a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">GitHub: <br><a href="https://github.com/${gitHub}" target="blank">${gitHub}</a></li>
+            </ul>
+            </div>
+        </div>`
+        }
+
+        console.log("team member added");
+        fs.appendFile("./dist/index.html", data, function(err){
+            if (err){
+                console.log(err)
+            }
+        })
+    });
 }
 
 function finishHTML (){
+    const finishHTML = `</body>
+    </html>`
+
+    fs.appendFile("./dist/index.html", finishHTML, function(err){
+        if (err){
+            console.log(err)
+        }
+    })
 }
 
+startHTML();
 addMember();
